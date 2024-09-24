b13e598c90a8a903dabcde86231c9ab40ffc8469
omarhsouna <omar.hsouna@sesame.com.tn>
2024-09-24 10:29:15 +0100

    initial config

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-[6000px]">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,7 @@
-"use client";
-import img1 from "./assets/images/img-1.jpg";
-import img2 from "./assets/images/img-2.jpg";
-import img3 from "./assets/images/img-3.jpg";
-import img4 from "./assets/images/img-4.jpg";
-import img5 from "./assets/images/img-5.jpg";
-import Section from "./components/Section";
+import React from "react";
 
-const images = [
-  {
-    id: 1,
-    src: img1,
-    title: "Image 1",
-  },
-  {
-    id: 2,
-    src: img2,
-    title: "Image 2",
-  },
-  {
-    id: 3,
-    src: img3,
-    title: "Image 3",
-  },
-  {
-    id: 4,
-    src: img4,
-    title: "Image 4",
-  },
-  {
-    id: 5,
-    src: img5,
-    title: "Image 5",
-  },
-];
+const Home = () => {
+  return <div>Home</div>;
+};
 
-export default function Home() {
-  return (
-    <>
-      {images.map((image, index) => (
-        <Section
-          key={image.id}
-          src={image.src}
-          title={image.title}
-          start={index * 1000}
-          end={(index + 1) * 1000}
-        />
-      ))}
-    </>
-  );
-}
+export default Home;
